7e26e3a87a13a45fe7271187b1c916e8935d714b
matsuteru82 <matsu_teru@y4.dion.ne.jp>
2025-09-02 22:57:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,16 @@ app = FastAPI()
 def read_root():
     return {"message": "Hello FastAPI"}
 
+#
+# 新しいエンドポイント
+#
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    """
+    item_id: パスパラメータ（必須, int型）
+    q: クエリパラメータ（任意, str型）
+    """
+    return {"item_id": item_id, "q": q}
+
 
 # https://github.com/matsuteru82/my-fastapi-project/tree/main
